ea6e141b1d8883d64f99b65c2353212a1ea17c08
Ariel Marcus <ariel@arielmarcus.com>
2022-05-27 11:48:05 -0400

    1.2.0a1

--- pyodide/build.sh
+++ pyodide/build.sh
@@ -1,1 +1,1 @@
-python -m build core && mv core/dist/dbt_core-1.0.1-py3-none-any.whl core/dist/dbt_core-1.0.1.4-py3-none-any.whl
+python -m build core && mv core/dist/dbt_core-1.2.0a1-py3-none-any.whl core/dist/dbt_core-1.2.0a1.1-py3-none-any.whl
